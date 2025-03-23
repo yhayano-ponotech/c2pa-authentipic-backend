93b4510e27174192e614df8a12f4ecc2b1cd6158
yhayano-ponotech <yhayano.biz@gmail.com>
2025-03-23 12:51:48 +0900

    CORSを全て受け入れに変更

--- src/config/index.ts
+++ src/config/index.ts
@@ -51,7 +51,7 @@ export const config = {
     }
   },
   cors: {
-    origin: process.env.CORS_ORIGIN || '*',
+    origin: '*',
     methods: ['GET', 'POST', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
